68e97c16bb6d39f4512073f00ac4b9aa3e27d0da
Rashitov Ilyas <rashtv.19@gmail.com>
2023-05-03 01:59:06 +0600

    Media functionality & Pictures for restaurants added

--- FoodService/restaurants/admin.py
+++ FoodService/restaurants/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Category, Food, Restaurant, BasketItem, Order
+from .models import RestaurantCategory, FoodCategory, Food, Restaurant, BasketItem, Order
 
-admin.site.register(Category)
+admin.site.register(RestaurantCategory)
+admin.site.register(FoodCategory)
 admin.site.register(Food)
 admin.site.register(Restaurant)
 admin.site.register(BasketItem)
